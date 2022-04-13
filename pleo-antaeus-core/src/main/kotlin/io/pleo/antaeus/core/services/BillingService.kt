8a11ddba06ecc4a80ea4b47b6314a266949d8dab
NinnOgTonic <git@espensen.me>
2022-04-13 11:58:47 +0200

    BillingService: Initial version using a timer to periodically check and charge pending invoices

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,65 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import mu.KotlinLogging
+import java.util.Timer
+import kotlin.concurrent.timerTask
+
+private val logger = KotlinLogging.logger {}
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val invoiceService: InvoiceService,
+    private val batchSize: Int = 100,
+    private val interval: Long = 2000
 ) {
-// TODO - Add code e.g. here
+    private val billingTask: Timer = Timer()
+
+    fun run() {
+        billingTask.scheduleAtFixedRate(timerTask { runBillingBatch() }, interval, interval)
+    }
+
+    internal fun runBillingBatch() {
+
+        val unpaidInvoices = invoiceService.fetchUnpaid(batchSize)
+
+        var successfullyCharged: MutableList<Invoice> = mutableListOf()
+
+        for (invoice in unpaidInvoices) {
+            // Ensure that the invoice status is really pending, as a sanity check in addition to that in our query
+            if (invoice.status != InvoiceStatus.PENDING) {
+                logger.error("Attempted to bill non-pending invoice: $invoice")
+                continue
+            }
+
+            // TODO: Consider optimistic locking pattern to allow multiple processors?
+            try {
+                /** TODO: Consider adding a processing step for the toy project, such that if our service is stopped unexpectedly we are able to send those stuck in the processing state to manual handling.
+                 * Realistically this would likely be handled by splitting the transaction into an authorizing stage and capturing payment stage?
+                 */
+                if (paymentProvider.charge(invoice)) {
+                    successfullyCharged.add(invoice)
+                } else {
+                    // Leave as pending, to retry later
+                    logger.trace { "Failed to charge: $invoice" }
+                }
+            } catch (e: NetworkException) {
+                // Leave as pending, to retry later
+                logger.trace("Network error when calling payment provider: $e")
+            } catch (e: Exception) {
+                logger.error("Unhandled error when calling payment provider: $e, Invoice: $invoice")
+            }
+        }
+
+        // TODO: Consider adding a lastAttemptedAt timer to failed transactions, to allow for logic not to try before some delay has passed
+
+        if (successfullyCharged.isNotEmpty()) {
+            // TODO: Processing could be sped up by signaling that we should re-run the function right away if count(unpaidInvoices) == batchSize
+            invoiceService.markAsPaid(successfullyCharged)
+            logger.debug("Successfully charged: $successfullyCharged")
+        }
+    }
 }
---
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -0,0 +1,159 @@
+package io.pleo.antaeus.core.services
+
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.pleo.antaeus.core.exceptions.NetworkException
+import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Money
+import org.junit.jupiter.api.Test
+
+class BillingServiceTest {
+    private val paymentProvider = mockk<PaymentProvider> {
+        every { charge(any()) } returns true
+    }
+
+    @Test
+    fun `should process payments successfully`() {
+        // Setup
+        val testInvoices = listOf(
+            Invoice(
+                1,
+                2,
+                Money(10.toBigDecimal(), Currency.EUR),
+                InvoiceStatus.PENDING
+            ),
+            Invoice(
+                5,
+                10,
+                Money(55.toBigDecimal(), Currency.USD),
+                InvoiceStatus.PENDING
+            )
+        )
+
+        val dal = mockk<AntaeusDal> {
+            every { fetchUnpaidInvoices(2) } returns testInvoices
+            every { updateInvoicesStatus(any(), any()) } returns Unit
+        }
+
+        val invoiceService = InvoiceService(dal = dal)
+        val billingService =
+            BillingService(
+                paymentProvider = paymentProvider,
+                invoiceService = invoiceService,
+                batchSize = 2,
+                interval = 2000
+            )
+
+        // Execute
+        billingService.runBillingBatch()
+
+        // Verify test
+        verify { paymentProvider.charge(testInvoices[0]) }
+        verify { paymentProvider.charge(testInvoices[1]) }
+
+        verify { dal.fetchUnpaidInvoices(2) }
+        verify { dal.updateInvoicesStatus(testInvoices, InvoiceStatus.PAID) }
+
+        confirmVerified()
+    }
+
+
+    @Test
+    fun `should skip already paid invoice status`() {
+        // Setup
+        val testInvoices = listOf(
+            Invoice(
+                1,
+                2,
+                Money(10.toBigDecimal(), Currency.EUR),
+                InvoiceStatus.PENDING
+            ),
+            Invoice(
+                5,
+                10,
+                Money(55.toBigDecimal(), Currency.USD),
+                InvoiceStatus.PAID
+            )
+        )
+
+        val dal = mockk<AntaeusDal> {
+            every { fetchUnpaidInvoices(2) } returns testInvoices
+            every { updateInvoicesStatus(any(), any()) } returns Unit
+        }
+
+        val invoiceService = InvoiceService(dal = dal)
+        val billingService =
+            BillingService(
+                paymentProvider = paymentProvider,
+                invoiceService = invoiceService,
+                batchSize = 2,
+                interval = 2000
+            )
+
+        // Execute
+        billingService.runBillingBatch()
+
+        // Verify test
+        verify { paymentProvider.charge(testInvoices[0]) }
+        verify(exactly = 0) { paymentProvider.charge(testInvoices[1]) }
+
+        verify { dal.updateInvoicesStatus(listOf(testInvoices[0]), InvoiceStatus.PAID) }
+
+        confirmVerified()
+    }
+
+    @Test
+    fun `should handle failed charges`() {
+        // Setup
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(match { it.id == 1 }) } returns false
+            every { charge(match { it.id == 5 }) } throws NetworkException()
+        }
+
+        val testInvoices = listOf(
+            Invoice(
+                1,
+                2,
+                Money(10.toBigDecimal(), Currency.EUR),
+                InvoiceStatus.PENDING
+            ),
+            Invoice(
+                5,
+                10,
+                Money(55.toBigDecimal(), Currency.USD),
+                InvoiceStatus.PENDING
+            )
+        )
+
+        val dal = mockk<AntaeusDal> {
+            every { fetchUnpaidInvoices(2) } returns testInvoices
+            every { updateInvoicesStatus(any(), any()) } returns Unit
+        }
+
+        val invoiceService = InvoiceService(dal = dal)
+        val billingService =
+            BillingService(
+                paymentProvider = paymentProvider,
+                invoiceService = invoiceService,
+                batchSize = 2,
+                interval = 2000
+            )
+
+        // Execute
+        billingService.runBillingBatch()
+
+        // Verify test
+        verify { paymentProvider.charge(testInvoices[0]) }
+        verify { paymentProvider.charge(testInvoices[1]) }
+
+        verify(exactly = 0) { dal.updateInvoicesStatus(any(), any()) }
+
+        confirmVerified()
+    }
+}
